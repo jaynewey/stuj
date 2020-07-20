9c1d3b3b824bd7a0d80d001484a114c329a40d45
Jay Newey <jayluxray@gmail.com>
2020-07-20 19:26:56 +0100

    Implement Family

--- src/Family.java
+++ src/Family.java
@@ -1,2 +1,19 @@
-public class Family {
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Family implements Iterable<Entity> {
+    private HashSet<Entity> entities = new HashSet<>();
+
+    public Family(HashSet<Entity> entities) {
+        this.entities = entities;
+    }
+
+    public void setEntities(HashSet<Entity> entities) {
+        this.entities = entities;
+    }
+
+    @Override
+    public Iterator<Entity> iterator() {
+        return entities.iterator();
+    }
 }
