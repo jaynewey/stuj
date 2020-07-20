4cf603e4c66046908367151c625a263ff70b2f29
Jay Newey <jayluxray@gmail.com>
2020-07-20 19:29:50 +0100

    Annotate Family

--- src/Family.java
+++ src/Family.java
@@ -2,13 +2,19 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 public class Family implements Iterable<Entity> {
-    private HashSet<Entity> entities = new HashSet<>();
+    /**
+     * Data structure for tracking sets of entities that have specific Component types.
+     */
+    private HashSet<Entity> entities;
 
     public Family(HashSet<Entity> entities) {
         this.entities = entities;
     }
 
     public void setEntities(HashSet<Entity> entities) {
+        /*
+         * Sets this Family's entities
+         */
         this.entities = entities;
     }
 
