fc9ffb515a9089b592627d3c1b7b4cc362200c42
Jay Newey <jayluxray@gmail.com>
2020-07-20 21:14:21 +0100

    Implement families

--- src/EntityManager.java
+++ src/EntityManager.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class EntityManager {
     private HashMap<Class<? extends Component>, HashMap<Entity, Component>> components = new HashMap<>();
     private HashMap<Entity, HashMap<Class<? extends Component>, Component>> entities = new HashMap<>();
+    private HashMap<HashSet<Class<? extends Component>>, Family> families = new HashMap<>();
     private LinkedList<EntitySystem> systems = new LinkedList<>();
 
     public EntityManager () {
@@ -54,6 +55,23 @@ public class EntityManager {
 
     public void removeSystem(EntitySystem system) {
         systems.remove(system);
+    }
+
+    public Family getFamily(HashSet<Class<? extends Component>> componentTypes) {
+        if (families.containsKey(componentTypes)) {
+            return families.get(componentTypes);
+        }
+        HashSet<Entity> entities = new HashSet<>();
+        for (Class<? extends Component> componentType: componentTypes) {
+            if (entities.isEmpty()) {
+                entities.addAll(components.get(componentType).keySet());
+            } else {
+                entities.retainAll(components.get(componentType).keySet());
+            };
+        }
+        Family family = new Family(entities);
+        families.put(componentTypes, family);
+        return family;
     }
 
     public void update(float deltatime) {
