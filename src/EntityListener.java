dc5d55c6fc0ed578fdfe233cc7a81e2cb78f26c7
Jay Newey <jayluxray@gmail.com>
2020-07-26 20:49:46 +0100

    Annotate EntityListener

--- src/EntityListener.java
+++ src/EntityListener.java
@@ -2,6 +2,12 @@ import java.util.HashMap;
 
 public interface EntityListener {
     void entityAdded(Entity entity);
+    /*
+    This method gets called when an entity is added to a manager this listener is registered to.
+     */
 
     void entityRemoved(Entity entity, HashMap<Class<? extends Component>, Component> components);
+    /*
+    This method gets called when an entity is removed from a manager this listener is registered to.
+     */
 }
