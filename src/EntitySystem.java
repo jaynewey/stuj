e22521594ca6bff42d1cd7aeafd2be9aea62993e
Jay Newey <jayluxray@gmail.com>
2020-08-01 12:13:02 +0100

    Annotate EntitySystem

--- src/EntitySystem.java
+++ src/EntitySystem.java
@@ -1,5 +1,12 @@
+/**
+ * Abstract class for processing Entity instances.
+ */
 public abstract class EntitySystem {
     public Family family;
 
+    /**
+     * The update method that is called every tick.
+     * @param deltatime Time between frames. Can be used for framerate independence.
+     */
     public abstract void update (float deltatime);
 }
