27fb0cee97941a1f53b9bd72c667e3abd6264cf0
Jay Newey <jayluxray@gmail.com>
2020-08-01 12:19:31 +0100

    Annotate EntityListener

--- src/EntityListener.java
+++ src/EntityListener.java
@@ -1,13 +1,19 @@
 import java.util.HashMap;
 
+/**
+ * Interface to listen to events broadcast by an EntityManager.
+ */
 public interface EntityListener {
+    /**
+     * This method gets called when an entity is added to a manager this listener is registered to.
+     * @param entity The entity that was added.
+     */
     void entityAdded(Entity entity);
-    /*
-    This method gets called when an entity is added to a manager this listener is registered to.
-     */
 
-    void entityRemoved(Entity entity, HashMap<Class<? extends Component>, Component> components);
-    /*
-    This method gets called when an entity is removed from a manager this listener is registered to.
+    /**
+     * This method gets called when an entity is removed from a manager this listener is registered to.
+     * @param entity The entity that was removed.
+     * @param components The map of components that were attached to the entity in the system.
      */
+    void entityRemoved(Entity entity, HashMap<Class<? extends Component>, Component> components);
 }
